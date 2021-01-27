b129e43956bed3ae87720bfc56213188754d4404
Dmitrii Tikhomirov <chani.liet@gmail.com>
2021-01-27 19:31:46 +0100

    additional tests

--- tests/src/test/java/org/treblereel/j2cl/shim/ES6ModuleTest.java
+++ tests/src/test/java/org/treblereel/j2cl/shim/ES6ModuleTest.java
@@ -14,6 +14,8 @@ public class ES6ModuleTest {
 
     private ES6Test tested = new ES6Test();
     private ES6Test2 testedWithJsName = new ES6Test2();
+    private ES6TransitiveTest es6TransitiveTest = new ES6TransitiveTest();
+    private ES6TestUserDefinedEs6ModuleAnnotation es6TestUserDefinedEs6ModuleAnnotation = new ES6TestUserDefinedEs6ModuleAnnotation();
 
     @Test
     public void testBooleanMethod() {
@@ -25,5 +27,9 @@ public class ES6ModuleTest {
     public void testClassStringProperty() {
         assertEquals("#id", tested.id);
         assertEquals("#id2", testedWithJsName.id);
+        assertEquals("#id", es6TransitiveTest.getEs6Testid());
+        assertEquals("#id2", es6TransitiveTest.getEs6Test2id());
+
+        assertEquals("#es6testuserdefinedes6moduleannotation", es6TestUserDefinedEs6ModuleAnnotation.id);
     }
 }
